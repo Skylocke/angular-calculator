cb267a1d7cf195e1da48eb70c70141dc88595e69
skylocke <mathematical.fiction@gmail.com>
2017-01-17 16:01:22 -0800

    calculator done. May consider DRYing html and grabFunctions()

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 console.log('app.js was loaded.');
+
+var app = angular.module("calculator", [])
+.controller("calcController", ["$scope", function($scope) {
+  $scope.readout = "0";
+
+  $scope.grabNumber = function(number) {
+    if($scope.readout === "0") {
+      $scope.readout = number;
+    } else {
+      $scope.readout += number;
+    }
+  };
+
+  $scope.grabOperation = function(operation) {
+    if($scope.readout[$scope.readout.length-1] !== " ") {
+      $scope.readout += (" " + operation + " ");
+    }
+  };
+
+  $scope.grabEquals = function() {
+    $scope.readout = eval($scope.readout).toString();
+  }
+
+  $scope.grabClear = function() {
+    $scope.readout = "0";
+  }
+
+}]);
